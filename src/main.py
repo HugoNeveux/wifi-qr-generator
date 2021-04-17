855923f1ee8024606a620cb3c95a18e3bfe835e8
HugoNeveux <hugoneveux@orange.fr>
2021-04-17 18:18:22 +0200

    Add --show-password option, to display Wi-Fi password

--- src/main.py
+++ src/main.py
@@ -32,8 +32,10 @@ def print_success(msg: str) -> None:
 def main() -> None:
     parser = agp.ArgumentParser(description="Parse command line arguments")
     parser.add_argument('--output', '-o', type=str, default="wifi_QR.png", help="Output path for QR code")
-    parser.add_argument('--ssid', type=str, default=None)
+    parser.add_argument('--ssid', type=str, default=None, help="Create QR code for Wi-Fi with provided SSID.")
+    parser.add_argument('--show-password', action='store_true', default=False, help="Print Wi-Fi password")
     args = parser.parse_args()
+    ssid, psk = "", ""
 
     if args.ssid is None:
         try:
@@ -42,16 +44,19 @@ def main() -> None:
             print_error("The system has no wifi connection; couldn't find current SSID. Please use the --ssid option.")
         except DependencyNotFoundError:
             print_error("This program only works with NetworkManager.")
-
     else:
         ssid = args.ssid
+
     try:
         psk = wifi.get_psk(ssid)
     except DependencyNotFoundError:
         print("This program only works with NetworkManager")
 
     qr.generate(ssid, psk, args.output)
-    print_success(f"Successfully generated QR code for {ssid}, as {args.output}")
+    print_success(f"Successfully generated QR code for {ssid} as {args.output}")
+
+    if args.show_password:
+        print(f"Current password for {ssid} : {psk}")
 
 
 if __name__ == "__main__":
