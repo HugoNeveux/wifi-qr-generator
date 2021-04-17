c583a036d144211f165533cb8d731cca4782d87f
HugoNeveux <hugoneveux@orange.fr>
2021-04-17 16:30:42 +0200

    Add QR code generator function

---
+++ src/qr.py
@@ -0,0 +1,17 @@
+# coding: utf8
+
+import qrcode
+
+
+def generate(ssid: str, psk: str, output: str = "wifi_QR.png") -> None:
+    """
+    Generate QR code used to connect to wifi network
+
+    :param ssid: network SSID
+    :param psk: network password
+    :param output: file name and path
+    :return:
+    """
+    data = f"WIFI:S:{ssid};T:WPA;P:{psk};;"
+
+    qrcode.make(data).save(output)
